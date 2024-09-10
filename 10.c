d2ee1f8200592d9358ce3476feebb320239875cf
Shweta Mathanker <135358941+Shweta-Mathanker@users.noreply.github.com>
2024-09-10 17:02:26 +0530

    Add files via upload

---
+++ 10.c
@@ -0,0 +1,14 @@
+    #include<stdio.h>
+int main(){
+    int age;
+    printf("age do:");
+    scanf("%i",&age);
+    if(age>=18){
+        printf("elligible for vote");
+    }
+    else{
+        printf("not elligible for vote");
+    }
+    return 0;
+    
+}
